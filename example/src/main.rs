20060d367cb830f6097fbd525e251135dd19e738
Caleb Winston <calebhwin@gmail.com>
2019-04-09 13:31:10 -0700

    fixed indentation

--- example/src/main.rs
+++ example/src/main.rs
@@ -27,19 +27,19 @@ emu! {
 		global_buffer[get_global_id(0)] *= scalar;
 	}
 
-    // multiply 2 matrices
-    // n is the dimension of the matrices
-    // a and b are the matrices to be multiplied, c is the result
-    multiply_matrices(n i32, global_a [f32], global_b [f32], global_c [f32]) {
-        // indices of cells to multiply
-        let i: i32 = get_global_id(0);
-        let j: i32 = get_global_id(1);
+	// multiply 2 matrices
+	// n is the dimension of the matrices
+	// a and b are the matrices to be multiplied, c is the result
+	multiply_matrices(n i32, global_a [f32], global_b [f32], global_c [f32]) {
+		// indices of cells to multiply
+		let i: i32 = get_global_id(0);
+		let j: i32 = get_global_id(1);
 
-        // execute step of multiplication
-        for k in 0..n {
-            global_c[i * n + j] += global_a[i * n + k] * global_b[k * n + j];
-        }
-    }
+		// execute step of multiplication
+		for k in 0..n {
+			global_c[i * n + j] += global_a[i * n + k] * global_b[k * n + j];
+		}
+	}
 }
 
 /// Expanded version with explanations.
@@ -51,35 +51,35 @@ emu! {
 ///
 #[allow(dead_code)]
 fn trivial() -> ocl::Result<()> {
-    // (1) Create an all-in-one context, program, command queue, and work /
-    // buffer dimensions:
-    let pro_que = ProQue::builder()
-        .src(EMU)
-        .dims(1 << 20)
-        .build()?;
+	// (1) Create an all-in-one context, program, command queue, and work /
+	// buffer dimensions:
+	let pro_que = ProQue::builder()
+        	.src(EMU)
+	        .dims(1 << 20)
+        	.build()?;
 
-    // (2) Create a `Buffer`:
-    let buffer = pro_que.create_buffer::<f32>()?;
+	// (2) Create a `Buffer`:
+	let buffer = pro_que.create_buffer::<f32>()?;
 
-    // (3) Create a kernel with arguments matching those in the source above:
-    let kernel = pro_que.kernel_builder("multiply")
-        .arg(&buffer)
-        .arg(&10.0f32)
-        .build()?;
+	// (3) Create a kernel with arguments matching those in the source above:
+	let kernel = pro_que.kernel_builder("multiply")
+		.arg(&buffer)
+		.arg(&10.0f32)
+		.build()?;
 
-    // (4) Run the kernel:
-    unsafe { kernel.enq()?; }
+	// (4) Run the kernel:
+	unsafe { kernel.enq()?; }
 
-    // (5) Read results from the device into a vector:
-    let mut vec = vec![0.0f32; buffer.len()];
-    buffer.read(&mut vec).enq()?;
+	// (5) Read results from the device into a vector:
+	let mut vec = vec![0.0f32; buffer.len()];
+	buffer.read(&mut vec).enq()?;
 
-    // Print an element:
-    println!("The value at index [{}] is now '{}'!", 200007, vec[200007]);
-    Ok(())
+	// Print an element:
+	println!("The value at index [{}] is now '{}'!", 200007, vec[200007]);
+	Ok(())
 }
 
 
 fn main() {
-    trivial().unwrap();
+	trivial().unwrap();
 }
