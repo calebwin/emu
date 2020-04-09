e93f2fa636f9a31f6b9a7c4476f34f28472e5df3
Caleb Winston <calebhwin@gmail.com>
2020-04-09 10:28:59 -0700

    Update docs

--- emu_core/build_docs.sh
+++ emu_core/build_docs.sh
@@ -1,5 +1,5 @@
 # run this from the emu_core directory
-cargo +nightly rustdoc -p emu_core --target-dir ../docs -- --disable-per-crate-search -Z unstable-options
+cargo +nightly rustdoc -p emu_core --target-dir ../docs --features glsl-compile -- --disable-per-crate-search -Z unstable-options
 rm ../docs/index.html
 cp ../docs/doc/emu_core/index.html ../docs/index.html
 sed -i 's/href="/href="doc\/emu_core\//g' ../docs/index.html
